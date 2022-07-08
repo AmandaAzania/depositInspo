1ce2e8faac986ae3278ec7cf3c2ef6f66f81231d
MuddathirDawood <dmuddathir@gmail.com>
2022-07-08 16:20:36 +0200

    home page

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+      <div class="container">
+        <img src="../assets/logo.png" alt="">
+        <p>depositInspo is a Lorem ipsum dolor, sit amet consectetur adipisicing elit. At veniam blanditiis officia repudiandae. Aliquid laudantium quia impedit dolore a similique.</p>
+        <button>Explore</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container{
+  min-width: 100vw;
+  margin: 0;
+  margin-top: 30px;
+  padding: 0;
+  /* display: flex;
+  flex-flow: column wrap;
+  justify-content: center; */
+}
+
+img{
+    width: 450px;
+    height: 450px;
+    border-radius: 100%;
+}
+
+p{
+    margin-top: 15px;
+}
+
+button{
+    width: 150px;
+    height: 35px;
+    border-radius: 25px;
+    border: white;
+}
+
+button:hover{
+    box-shadow: 3px 3px 5px black;
+    transition: all 1s;
+}
+
+</style>
