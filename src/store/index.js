ffa8410407cb27131969a2d7fb1c7b2ce57fc300
Finn818 <jsolomons239@gmail.com>
2022-07-13 10:10:01 +0200

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,58 @@
 import { createStore } from 'vuex'
+// import Vue from 'vue';
+// import Vuex from 'vuex';
+import db from 'db.json';
+
+// Vue.useAttrs(Vuex);
 
 export default createStore({
   state: {
+    users: [],
+    currentUser: {name: 'Text User'}
   },
-  getters: {
-  },
+  
+
   mutations: {
+    SET_USERS(state, users) {
+      state.users = users;
+    },
+    LOGOUT_USER(state) {
+      state.currentUser = {}
+      window.localStorage.currentUser = JSON.stringify({});
+    },
+    SET_CURRENT_USER(state, user) {
+      state.currentUser = user;
+      window.localStorage.currentUser = JSON.stringify(user);
+    },
   },
+
   actions: {
-  },
+    login: async (context, payload) => {
+      const { email, password } = payload;
+      const response = await fetch(
+        `http://localhost:3000/users?email=${email}&password=${password}`
+      );
+      const userData = await response.json();
+      context.commit("user", userData[0]);
+    },
+    register: (context, payload) => {
+      const { name, email, password } = payload;
+      fetch("", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => context.commit("user", json));
+    }},
+  
   modules: {
   }
 })
+
